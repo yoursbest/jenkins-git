f6e9e50224d2213d2b3197210255829659fc8126
yoursbest <xwliao@outlook.com>
2020-05-29 16:58:52 +0800

    Create types.py

---
+++ types.py
@@ -0,0 +1,11 @@
+from decimal import *
+
+x = 7 > 5
+y = "seven".capitalize()
+z = 'seven "{1:<9}" "{0:>09}"'.format(8, 9)
+print("x is {}".format(x))
+print(f"{y}")
+print(f"{z}")
+print(type(x))
+print(type(y))
+print(type(z))
